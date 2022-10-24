3d5e7606bf207e5b94210a63298d2fdc0c8af136
Daniel Murillo <sittingbout@gmail.com>
2022-10-24 18:06:57 +0200

    first steps

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import MovieBox from './components/MovieBox';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=1976c380dd1c386feb7c2778eef34284";
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch(API_URL).then((res) => res.json()).then(data => {
+      console.log(data);
+      setMovies(data.results);
+    })
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      {movies.map((movieReq) => <MovieBox key={movieReq.id}{...movieReq} />)}
     </div>
   );
 }
---
+++ src/components/MovieBox.js
@@ -0,0 +1,17 @@
+import React from 'react'
+const API_IMG = "https://image.tmdb.org/t/p/w500/";
+
+const MovieBox = ({ title, poster_path, vote_average, realease_date, overview }) => {
+    return (
+        <div className='card text-center bg-secondary mb-3'>
+            <div className='card-body'>
+                <img className='card-img-top' src={API_IMG + poster_path} alt="" />
+                <div className='card-body'>
+                    <button type='button' className='btn btn-dark'>View More</button>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+export default MovieBox;
